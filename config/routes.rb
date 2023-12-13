83ae5aee835721575307e7589718780297106b99
scharriere14 <stephaniescharriere@gmail.com>
2023-12-13 16:56:16 -0600

    Reorginized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
-Rails.application.routes.draw do
-  devise_for :customers
-  get 'search/index'
+# config/routes.rb
 
+Rails.application.routes.draw do
+  # Devise for customers
+  devise_for :customers, controllers: { sessions: 'customers/sessions' }
+
+  # ActiveAdmin for admin users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  # Resources
   resources :addresses
   resources :customers
   resources :orders
@@ -18,22 +22,11 @@ Rails.application.routes.draw do
     get 'remove_from_cart', on: :member
   end
 
+  # Routes related to products
   get 'result/:id', to: 'products#show', as: 'result'
-  get "up" => "rails/health#show", as: :rails_health_check
-  get '/search', to: 'search#index', as: 'search'
-  root 'about#index'
-
   post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
   get 'products/add_to_cart/:id', to: 'products#add_to_cart' # Because of bugs
   get 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart' # Because of bugs
 
-  # Stripe stuff
-  scope '/checkout' do
-    post '/create', to: 'checkout#create', as: "checkout_create"
-    # get '/create.:id', to: 'checkout#create', as: 'checkout_create2'
-    get '/create', to: 'checkout#create', as: 'checkout_create2'
-
-    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
-    get 'success', to: 'checkout#success', as: 'checkout_success'
-  end
-end
+  # Health check route
+  get "up" =>
